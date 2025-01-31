50b05ba96aa3373b4a01e1473e9aed100b6166c3
gustavo Almeida <gu.almeidan2007@gmail.com>
2025-01-31 11:49:00 -0300

    Remove Element (comments)

--- RemoveElement.ts
+++ RemoveElement.ts
@@ -1,30 +1,31 @@
 /*
-
+ to start, 
 
 
 
 */
 
 function removeElement(nums: number[], val: number): number {
-    let index = 0;
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] !== val) {
-            nums[index] = nums[i];
-            index++;
+    let index = 0; // Pointer to track the position of non-val elements
+    
+    for (let i = 0; i < nums.length; i++) { // Loop through the entire array
+        if (nums[i] !== val) { // If the current element is not equal to val
+            nums[index] = nums[i]; // Move it to the position tracked by index
+            index++; // Increment the index for the next non-val element
         }
     }
-    return index;
+    return index; // Return the new length of the array without val elements
 }
 
 function moveToEnd(nums: number[], val: number): number[] {
-    let newLength = removeElement(nums, val);
-    let count = nums.length - newLength;
+    let newLength = removeElement(nums, val); // Get the new length after removing val elements
+    let count = nums.length - newLength; // Count how many val elements were removed
     
-    while (count > 0) {
-        nums[newLength] = val;
-        newLength++;
-        count--;
+    while (count > 0) { // Append val elements to the end of the array
+        nums[newLength] = val; // Assign val to the remaining positions
+        newLength++; // Move the index forward
+        count--; // Decrease the remaining count
     }
     
-    return nums;
+    return nums; // Return the modified array
 }
